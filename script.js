db8c5bd3919b79be9f07ebc6571ef4afad4a15b6
ElkhanKarimov <elxan0409@gmail.com>
2024-02-20 03:59:26 +0400

    complated task

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+// const dragableImage = document.getElementById("dragableImage")
+
+// dragableImage.addEventListener("dragstart", (e) => {
+//     e.dataTransfer.setData("text", e.target.id)
+// })
+
+// const box = document.querySelectorAll(".box")
+
+// box.forEach((box) => {
+//     box.addEventListener("dragover", (e) => {
+//         e.preventDefault()
+//     })
+//     box.addEventListener("drop", (e) => {
+//         e.preventDefault()
+
+//         const data = e.dataTransfer.getData("text")
+//         const dragableElement = document.getElementById(data)
+//         box.appendChild(dragableElement)
+//     })
+
+
+// })
